5e7793d0ce06f9d6bba143cd77220ffe35d93b1a
VatsalSheth <vash3665@colorado.edu>
2019-07-18 02:33:57 -0600

    Ex4 q3 comment

--- Exercise_4/Ques 3/ques3.cpp
+++ Exercise_4/Ques 3/ques3.cpp
@@ -13,7 +13,7 @@
 #include <string>
 
 #include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui/highgui.hpp> 
 #include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
