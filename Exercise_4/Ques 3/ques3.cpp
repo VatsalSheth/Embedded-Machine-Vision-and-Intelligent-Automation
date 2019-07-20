f3fc6d4581d9d9a8ef81383d3e5b252b886362f5
VatsalSheth <vash3665@colorado.edu>
2019-07-19 22:55:41 -0600

    timing added

--- Exercise_4/Ques 3/ques3.cpp
+++ Exercise_4/Ques 3/ques3.cpp
@@ -19,6 +19,61 @@
 using namespace cv;
 using namespace std;
 
+double max_time = 0;
+double avg_time = 0;
+
+#define NSEC_PER_SEC (1000000000)
+
+int delta_t(struct timespec *stop, struct timespec *start, struct timespec *delta_t)
+{
+  int dt_sec=stop->tv_sec - start->tv_sec;
+  int dt_nsec=stop->tv_nsec - start->tv_nsec;
+
+  if(dt_sec >= 0)
+  {
+    if(dt_nsec >= 0)
+    {
+      delta_t->tv_sec=dt_sec;
+      delta_t->tv_nsec=dt_nsec;
+	  return(1);
+    }
+    else
+    {
+      delta_t->tv_sec=dt_sec-1;
+      delta_t->tv_nsec=NSEC_PER_SEC+dt_nsec;
+	  if(dt_sec-1>=0)
+		   return(1);
+	   else
+		   return(0);
+    }
+  }
+  else
+  {
+    if(dt_nsec >= 0)
+    {
+      delta_t->tv_sec=dt_sec;
+      delta_t->tv_nsec=dt_nsec;
+    }
+    else
+    {
+      delta_t->tv_sec=dt_sec-1;
+      delta_t->tv_nsec=NSEC_PER_SEC+dt_nsec;
+    }
+	return(0);
+  }
+}
+
+void frame_rate(struct timespec *delta_t)
+{
+	double delta_sec; 
+	
+	delta_sec = (double)delta_t->tv_sec+((double)delta_t->tv_nsec/(double)NSEC_PER_SEC);
+	avg_time += delta_sec;
+		
+	if(delta_sec > max_time)
+		max_time = delta_sec;
+}
+
 Mat skeletal(Mat src)
 {
 	Mat gray, binary, mfblur;
@@ -68,6 +123,8 @@ int main( int argc, char** argv )
     int c = 1;
 	string filename;
 
+	struct timespec start_time,end_time,delta_time;
+	
 	namedWindow("Capture Example", CV_WINDOW_AUTOSIZE);
 
     while(1)
@@ -75,16 +132,28 @@ int main( int argc, char** argv )
         frame=cvQueryFrame(capture);
 		if(!frame) break;
 
+		clock_gettime(CLOCK_REALTIME, &start_time);
+		
 		ip = cvarrToMat(frame);
 		op = ip;
 		op = skeletal(ip);
         
+        clock_gettime(CLOCK_REALTIME, &end_time);		
+		delta_t(&end_time, &start_time, &delta_time);
+		
+		if(c)
+			frame_rate(&delta_time);
+			
        	filename = "./frame" + to_string(c) + ".jpeg";
 		cout << filename << endl;
 		imwrite(filename.c_str(), op);
 		
-		if(c >= 3000)
+		if(c >= 300)
+		{
+			avg_time = avg_time / c;
+			printf("Average execution time is %f sec and Max execution time is %f sec\n", avg_time, max_time);
 			break;
+		}
 			
 		c++;
     }
