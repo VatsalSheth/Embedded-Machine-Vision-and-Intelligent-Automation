bb495c373dd329cfff7812ca181a7d5afe8317f5
Vatsal Sheth <vash3665@colorado.edu>
2019-08-13 23:40:14 -0700

    code update

--- Autonomus vehicle/project.cpp
+++ Autonomus vehicle/project.cpp
@@ -52,8 +52,14 @@ Rect crop_roi;
 int height_offset;
 String car_cascade_name = "cars.xml";
 CascadeClassifier car_cascade;
+String light_cascade_name = "traffic_light.xml"; 
+CascadeClassifier light_cascade;
 HOGDescriptor hog_pedestrian;
-
+//vector<Rect> found;
+//std::vector<Rect> cars;
+//Vec4i draw_left, draw_right;
+//std::vector<Rect> light;
+	
 // POSIX thread declarations and scheduling attributes
 pthread_t threads[NUM_THREADS];
 pthread_attr_t thread_attr;
@@ -142,15 +148,19 @@ void create_lane_mask(Mat src)
 }
 
 //Detect car using Haar cascade classifier and mark a rectangle surrounding it on original frame
-Mat detect_car(Mat src, Mat frame)
+Mat detect_car(Mat src, Mat frame, int index)
 {
-	Mat small_frame;
 	std::vector<Rect> cars;
-	
-	resize(src, small_frame, Size(0,0), 0.5, 0.5, INTER_LINEAR );				//Down size to 50 %
-	equalizeHist(small_frame, small_frame);			//Normalize histogram
 
-	car_cascade.detectMultiScale(small_frame, cars, 1.2, 3, 0, Size(30,30));
+	//if((index & 0x07) == 0)
+	{
+		Mat small_frame;
+	
+		resize(src, small_frame, Size(0,0), 0.5, 0.5, INTER_LINEAR );				//Down size to 50 %
+		equalizeHist(small_frame, small_frame);			//Normalize histogram
+
+		car_cascade.detectMultiScale(small_frame, cars, 1.2, 3, 0, Size(30,30));
+	}
 	
 	for( size_t i = 0; i < cars.size(); i++ )
     {
@@ -159,23 +169,54 @@ Mat detect_car(Mat src, Mat frame)
 		cars[i].width = (cars[i].width<<1);
 		cars[i].height = (cars[i].height<<1);
 		cv::rectangle(frame, cars[i], Scalar(255, 0, 255), 2, 8, 0);
+		putText(frame, "C", Point(cars[i].x, cars[i].y),  FONT_HERSHEY_PLAIN, 2, Scalar(255, 0, 255), 2, 8);
+    }
+    
+    return frame;
+}
+
+//Detect Traffic Light using Haar cascade classifier and mark a rectangle surrounding it on original frame
+Mat detect_light(Mat src, Mat frame, int index)
+{
+	std::vector<Rect> light;
+
+	//if((index & 0x07) == 0)
+	{
+		Mat small_frame;
+	
+		resize(src, small_frame, Size(0,0), 0.5, 0.5, INTER_LINEAR );				//Down size to 50 %
+		equalizeHist(small_frame, small_frame);			//Normalize histogram
+
+		light_cascade.detectMultiScale(small_frame, light, 1.2, 3, 0, Size(30,30));
+	}
+	
+	for( size_t i = 0; i < light.size(); i++ )
+    {
+		light[i].y = (light[i].y<<1);// + height_offset;
+		light[i].x = (light[i].x<<1);
+		light[i].width = (light[i].width<<1);
+		light[i].height = (light[i].height<<1);
+		cv::rectangle(frame, light[i], Scalar(0, 255, 0), 2, 8, 0);
+		putText(frame, "T", Point(light[i].x, light[i].y),  FONT_HERSHEY_PLAIN, 2, Scalar(0, 255, 0), 2, 8);
     }
     
     return frame;
 }
 
 //Detect pedestrian and mark a rectangle surrounding it on original frame
-Mat detect_pedestrian(Mat src, Mat frame)
+Mat detect_pedestrian(Mat src, Mat frame, int index)
 {
-	Mat small_frame;
+	//vector<Rect> found;
+	if((index & 0x07) == 0)
+	{
+		Mat small_frame;
 	
-	resize(src, small_frame, Size(0,0), 0.5, 0.5, INTER_LINEAR );				//Down size to 50 %
-	//equalizeHist(small_frame, small_frame);			//Normalize histogram
+		resize(src, small_frame, Size(0,0), 0.5, 0.5, INTER_LINEAR );				//Down size to 50 %
+		equalizeHist(small_frame, small_frame);			//Normalize histogram
+
+		hog_pedestrian.detectMultiScale(small_frame, found, 0, Size(8,8), Size(0,0), 1.05, 2, false);
+	}
 	
-	vector<Rect> found;
-
-    hog_pedestrian.detectMultiScale(small_frame, found, 0, Size(8,8), Size(32,32), 1.05, 2, false);
-
     // Draw bounding rectangle
     for( size_t i = 0; i < found.size(); i++ )
     {
@@ -184,69 +225,74 @@ Mat detect_pedestrian(Mat src, Mat frame)
 		found[i].width = (found[i].width<<1);
 		found[i].height = (found[i].height<<1);
 		rectangle(frame, found[i], cv::Scalar(0,0,255), 3);
+		putText(frame, "P", Point(found[i].x, found[i].y),  FONT_HERSHEY_PLAIN, 2, Scalar(0,0,255), 2, 8);
 	}
 	
 	return frame;
 }
 
 //Detect lane and mark them on original frame
-Mat detect_lane(Mat src, Mat frame)
+Mat detect_lane(Mat src, Mat frame, int index)
 {
-	Mat mask_frame, blur_frame, canny_frame;
-	double slope;
+	//cv::Point intersect;
 	Vec4i draw_left, draw_right;
-	
-	draw_left[0] = 0;
-	draw_left[1] = 0;
-	draw_left[2] = 0;
-	draw_left[3] = crop_roi.height;
-	draw_right = draw_left;
-	
-	blur(src, blur_frame, Size(3,3));	//Apply gaussian blur
-	Canny(blur_frame, canny_frame, 100, 200, 3);	//Detect edges using canny edge transform
-	
-	
-	canny_frame.copyTo(mask_frame, lane_mask);   //Apply the Triangular mask to detect lines only from region of interest
-	
-	#ifdef DEBUG
-		imshow("Lane detection: Edges on ROI", mask_frame);
-		cvWaitKey(1);
-	#endif
-	
-	vector<Vec4i> lines;
-	HoughLinesP(mask_frame, lines, 1, CV_PI/180, 150, 100, 10);		//Detect lines using probabilistic hough transform 
-			
-	for( size_t i = 0; i < lines.size(); i++ )
+	//if((index & 0x01) == 0)
 	{
-		Vec4i l = lines[i];
+		Mat mask_frame, blur_frame, canny_frame;
+		double slope;
+		vector<Vec4i> lines;
 		
-		slope = (l[3]-l[1])/((double)(l[2]-l[0]));				//calculate slope to remove almost horizontal lines from this list
-		if(slope>0.5) 	                        //Draw only those lines which satisfy this slope criteria
-		{	
-			if(l[1]>draw_right[1])
-			{
-				draw_right[1] = l[1];
-				draw_right[0] = l[0];
-			}
-			
-			if(l[3]<draw_right[3])
-			{
-				draw_right[2] = l[2];
-				draw_right[3] = l[3];
-			}		
-		}
-		else if(slope<(-0.5))
+		
+		draw_left[0] = 0;
+		draw_left[1] = 0;
+		draw_left[2] = 0;
+		draw_left[3] = crop_roi.height;
+		draw_right = draw_left;
+	
+		blur(src, blur_frame, Size(3,3));	//Apply gaussian blur
+		Canny(blur_frame, canny_frame, 100, 200, 3);	//Detect edges using canny edge transform
+	
+		canny_frame.copyTo(mask_frame, lane_mask);   //Apply the Triangular mask to detect lines only from region of interest
+	
+		#ifdef DEBUG
+			imshow("Lane detection: Edges on ROI", mask_frame);
+			cvWaitKey(1);
+		#endif
+	
+		HoughLinesP(mask_frame, lines, 1, CV_PI/180, 150, 10, 10);		//Detect lines using probabilistic hough transform 
+		
+		for( size_t i = 0; i < lines.size(); i++ )
 		{
-			if(l[1]>draw_left[1])
-			{
-				draw_left[1] = l[1];
-				draw_left[0] = l[0];
-			}
+			Vec4i l = lines[i];
 			
-			if(l[3]<draw_left[3])
+			slope = (l[3]-l[1])/((double)(l[2]-l[0]));				//calculate slope to remove almost horizontal lines from this list
+			if(slope>0.5) 	                        //Draw only those lines which satisfy this slope criteria
+			{	
+				if(l[1]>draw_right[1])
+				{
+					draw_right[1] = l[1];
+					draw_right[0] = l[0];
+				}
+			
+				if(l[3]<draw_right[3])
+				{
+					draw_right[2] = l[2];
+					draw_right[3] = l[3];
+				}		
+			}
+			else if(slope<(-0.5))
 			{
-				draw_left[2] = l[2];
-				draw_left[3] = l[3];
+				if(l[1]>draw_left[1])
+				{
+					draw_left[1] = l[1];
+					draw_left[0] = l[0];
+				}
+			
+				if(l[3]<draw_left[3])
+				{
+					draw_left[2] = l[2];
+					draw_left[3] = l[3];
+				}
 			}
 		}
 	}
@@ -256,6 +302,14 @@ Mat detect_lane(Mat src, Mat frame)
 	
 	if(draw_right[0] != 0)
 		line(frame, Point(draw_right[0], draw_right[1]+height_offset), Point(draw_right[2], draw_right[3]+height_offset), Scalar(0,0,255), 3, CV_AA);	 //Draw right lane. Add vertical offset to draw line on original full size frame
+	
+	/*if((draw_left[0] != 0) && (draw_right[0] != 0))
+	{
+		intersect.x = ((((draw_left[0]*draw_left[3]) - (draw_left[1]*draw_left[2]))*(draw_right[0]-draw_right[2])) - (((draw_right[0]*draw_right[3]) - (draw_right[1]*draw_right[2]))*(draw_left[0]-draw_left[2]))) / (((draw_left[0]-draw_left[2])*(draw_right[1]-draw_right[3])) - ((draw_left[1]-draw_left[3])*(draw_right[0]-draw_right[2])));
+		intersect.y = (((draw_left[0]*draw_left[3]) - (draw_left[1]*draw_left[2]))*(draw_right[1]-draw_right[3])) - (((draw_right[0]*draw_right[3]) - (draw_right[1]*draw_right[2]))*(draw_left[1]-draw_left[3])) / (((draw_left[0]-draw_left[2])*(draw_right[1]-draw_right[3])) - ((draw_left[1]-draw_left[3])*(draw_right[0]-draw_right[2])));
+		circle(frame, intersect, 20, Scalar(0,0,255), 5, 8, 0);
+		printf("Value x=%d, y=%d\n", intersect.x, intersect.y);
+	}*/	
 	
 	#ifdef DEBUG
 		imshow("Lane detection: Marked", frame);
@@ -274,6 +328,12 @@ int initialize_cascade()
 		return -1; 
 	}
 	
+	if(!light_cascade.load(light_cascade_name))
+	{ 
+		cout<<"Fail to load Traffic Light cascade xml file !!!\n"; 
+		return -1; 
+	}
+	
 	hog_pedestrian.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
 }
 
@@ -282,7 +342,7 @@ void *autopilot(void *threadp)
 {
 	string filename;
 	int idx;
-	Mat ip_frame, op_frame, gray_frame, crop_frame, lane_frame, ped_frame;
+	Mat ip_frame, op_frame, gray_frame, crop_frame, lane_frame, ped_frame, light_frame;
 	
     do
     {
@@ -295,14 +355,13 @@ void *autopilot(void *threadp)
 		if (ip_frame.empty())
 			break;
 		
-		//clock_gettime(CLOCK_REALTIME, &start_time);
-		
 		cvtColor(ip_frame, gray_frame, CV_BGR2GRAY); //Convert to gray scale
 		crop_frame = gray_frame(crop_roi);     //Crop frame to half
 		
-		lane_frame = detect_lane(crop_frame, ip_frame);
-		ped_frame = detect_pedestrian(gray_frame, lane_frame);
-		op_frame = detect_car(crop_frame, ped_frame);
+		lane_frame = detect_lane(crop_frame, ip_frame, idx);
+		ped_frame = detect_pedestrian(gray_frame, lane_frame, idx);
+		light_frame = detect_light(gray_frame, ped_frame, idx);
+		op_frame = detect_car(crop_frame, light_frame, idx);
 		
 		if(show_flag == 1)
 		{
@@ -313,10 +372,9 @@ void *autopilot(void *threadp)
 		if(store_flag == 1)
 		{
 			filename = "./Track_frame" + to_string(idx) + ".ppm";
-			//cout << filename << endl;
 			imwrite(filename.c_str(), op_frame); 
 		}
-	}while(exit_flag==false && (frame_idx<2000));
+	}while(exit_flag==false && (frame_idx<2300)); //To limit video length for testing purpose
 	
 	pthread_exit(NULL);
 }
@@ -401,11 +459,12 @@ int main(int argc, char **argv)
 	if(initialize_cascade()== -1)
 		return -1;
 	
-	while(frame_idx<750)
+	//To skip frames in starting
+	/*while(frame_idx<750)
 	{
 		vid >> tmp_frame; 
 		frame_idx++;
-	}
+	}*/
 	frame_idx = 1;
 	
 	num_proc = get_nprocs(); 
@@ -422,8 +481,6 @@ int main(int argc, char **argv)
 	if(rc < 0)
 		perror("main_param");
 	
-	clock_gettime(CLOCK_REALTIME, &start_time);
-	
 	for(int i=0; i<num_proc; i++)
 	{
 		CPU_ZERO(&threadcpu);
@@ -438,6 +495,8 @@ int main(int argc, char **argv)
 	
 		pthread_create(&threads[i], &thread_attr, autopilot, NULL);
 	}
+	
+	clock_gettime(CLOCK_REALTIME, &start_time);
 	
 	for(rc=0; rc<num_proc; rc++)
 	{
@@ -455,5 +514,5 @@ int main(int argc, char **argv)
 		system(ffmpeg_cmd.c_str());				//Generate ouyput video using ffmpeg
 	
 	avg_time = avg_time / frame_idx;			//Calculate average execution time
-	printf("Average execution time is %f sec and Max execution time is %f sec\n", avg_time, max_time);
+	printf("Average execution time is %f sec\n", avg_time);
 }
